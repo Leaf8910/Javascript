534de5580d1b9c6246be975bcf59c0c366fee652
Leaf8910 <100060220+Leaf8910@users.noreply.github.com>
2022-05-18 20:21:57 +0800

    Added images
    Redid the javascript script and html and added multiple containers

---
+++ Exercises/Exercise 5/myscript1.js
@@ -0,0 +1,14 @@
+
+// Naming User and Computer Score to update
+const userScore = document.querySelector('.user-score')
+const computerScore = document.querySelector('.computer-score')
+
+// Naming User buttons to add event listeners
+const rockButton = document.querySelector('[data-rock]')
+const paperButton = document.querySelector('[data-paper]')
+const scissorsButton = document.querySelector('[data-scissors]')
+ 
+// Naming Computer buttons to add event listeners
+const computerRock = document.querySelector('[data-computer-rock]')
+const computerPaper = document.querySelector('[data-computer-paper]')
+const computerScissors = document.querySelector('[data-computer-scissors]') 
